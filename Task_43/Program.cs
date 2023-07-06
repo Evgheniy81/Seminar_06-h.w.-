1f4330fb0fa1d2584ee9ac12b2bebef77f3d971f
Jeka_A <antimur4ik@yandex.ru>
2023-07-06 21:27:21 +0300

    Нахождение точки пересечения двух прямых

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -1,16 +1,18 @@
 ﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
 //уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
- Console.WriteLine("Введите b1: ");
- int b1 = Convert.ToInt32(Console.ReadLine());
- Console.WriteLine("Введите b2: ");
- int b2 = Convert.ToInt32(Console.ReadLine());
- Console.WriteLine("Введите k1: ");
- int k1 = Convert.ToInt32(Console.ReadLine());
- Console.WriteLine("Введите r2: ");
- int k2 = Convert.ToInt32(Console.ReadLine());
- double y = (k1 * x + b1) - (k2 * x + b2);
- for (int i = 0; i < length; i++)
- {
-    
- }
+
+Console.WriteLine("Введите b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+double x = (double) (b2 - b1) / (k1 - k2);
+//double y = (double) k1 * x + b1; 
+//double y2 = (double) k2 * x + b2;
+Console.WriteLine();
+Console.WriteLine(x);
+
